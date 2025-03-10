c010c1281bbd7ae1286f419560e087b5e920e326
M0jjmd <moamine517@gmail.com>
2025-03-10 16:41:45 +0100

    Added store and all components on portfolio

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,9 +1,13 @@
 import Introduction from '../components/Introduction'
+// import Projects from '../components/Projects'
+// import Contact from '../components/Contact'
 
 const Contacts = () => {
     return (
         <>
             <Introduction />
+            {/* <Projects />
+            <Contact /> */}
         </>
     )
 }
