9704e80c91cbd5ca543234935e4694b6f9bfc48e
M0jjmd <moamine517@gmail.com>
2025-03-21 20:15:06 +0100

    Added react-pdf to read cv

--- src/components/PdfViwer.tsx
+++ src/components/PdfViwer.tsx
@@ -4,18 +4,23 @@ import { useState } from "react"
 pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@3.6.172/build/pdf.worker.min.js`
 
 const PdfViewer = () => {
-    const [numPages, setNumPages] = useState(null)
+    const [numPages, setNumPages] = useState<number | null>(null)
+
+    const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+        setNumPages(numPages)
+    }
 
     return (
         <div className="flex flex-col items-center">
             <h2 className="text-xl font-bold mb-4">Visor de PDF</h2>
             <Document
                 file="/ejemplo.pdf"
-                onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+                onLoadSuccess={onDocumentLoadSuccess}
             >
-                {Array.from(new Array(numPages), (el, index) => (
-                    <Page key={index} pageNumber={index + 1} />
-                ))}
+                {numPages &&
+                    Array.from({ length: numPages }, (_, index) => (
+                        <Page key={index} pageNumber={index + 1} />
+                    ))}
             </Document>
         </div>
     )
